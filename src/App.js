2f976d48d47821367a11abec0d98ac87e978460a
adospinoiu <alex.dospinoiu@gmail.com>
2021-05-08 05:41:26 -0700

    Established the generic div-components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       
+        <div className="drag-n-drop">
+          <div className="dnd-group">
+            <div className="dnd-item">
+              <div>
+                <p>ITEM</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </header>
     </div>
   );
