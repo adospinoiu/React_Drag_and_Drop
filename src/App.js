f482e98a933e0bd917c546d3f2fe4f9d0192734b
adospinoiu <alex.dospinoiu@gmail.com>
2021-05-09 05:32:06 -0700

    Added data which will be passed dynamically to the div's

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import './App.css';
 
+const data = [
+  {title: 'group 1', items: ['1', '2', '3']},
+  {title: 'group 2', items: ['4', '5']}
+]
+
 function App() {
   return (
     <div className="App">
