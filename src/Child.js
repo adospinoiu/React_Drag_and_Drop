4d71c475b0ed8203adf2b130cd4e7b7692416466
adospinoiu <alex.dospinoiu@gmail.com>
2021-05-27 21:12:58 -0700

    Created the child component that will pass data to the parent

---
+++ src/Child.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Child() {
+    return (
+        <div>
+            <h1>Child</h1>
+            <button>Click to Change SOMETHING</button>
+        </div>
+    )
+}
+
+export default Child
