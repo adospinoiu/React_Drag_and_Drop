e667ba4c0d7e481dd84c745f5b4ab2d82f5cc664
adospinoiu <alex.dospinoiu@gmail.com>
2021-05-27 21:11:19 -0700

    Adding version where testing data movement

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import React from 'react';
-import './App.css';
 
+// ###### Drag-N-Drop CODE ######
+// import './App.css';
 // import DragNDrop from './components/DragNDrop'
-
 // const data = [
 //   {title: 'Group 1', items: ['1', '2', '3']},
 //   {title: 'Group 2', items: ['4', '5']}
 // ]
+// ###############################
+
+// ###### Moving Data from Child to Parent ######
+import './Parent.css'; 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        {/* <DragNDrop data={data}/> */}
+    // ###### Drag-N-Drop CODE ######
+    // <div className="App">
+    //   <header className="App-header">
+    //     <DragNDrop data={data}/>
+    //   </header>
+    // </div>
+    // ##############################
 
-       
-      </header>
+    <div className="parent">
+        <h1>Parent</h1>
     </div>
-  );
+  )
 }
 
 export default App;
